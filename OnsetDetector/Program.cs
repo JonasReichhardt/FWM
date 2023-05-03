d9c6ff3184c6c01071e23b0c0ddefeeeace4fc03
shaslhofer <haslhofer@unisoftwareplus.com>
2023-05-03 23:20:32 +0200

    initial commit

---
+++ OnsetDetector/Program.cs
@@ -0,0 +1,15 @@
+﻿using AForge.Math;
+using NAudio.Wave;
+
+using (AudioFileReader reader = new AudioFileReader("./Ressources/ff123_kraftwerk.wav"))
+{
+    var mono = reader.ToMono();
+
+    int fps = 70;
+
+    Complex[] Array = new Complex[2048];
+    /*******fill the array************/
+    FourierTransform.FFT(Array, FourierTransform.Direction.Forward);
+
+    WaveFileWriter.CreateWaveFile16("Test.wav", mono);
+}
